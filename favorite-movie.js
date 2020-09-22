b54b2473701ee7fb5f40ff31d86759de5ca877c1
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-09-22 10:42:00 +0200

    Edition of the message required by the quest

---
+++ favorite-movie.js
@@ -0,0 +1,6 @@
+const movieTitle = 'Back to the future';
+const year = 1985;
+const directorName = 'Robert Zemeckis';
+const message = movieTitle + ", directed by " + directorName + ", was released in " + year + ".";
+
+alert(message);
